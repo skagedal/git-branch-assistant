15d4b0d02ab60112f88335f90cff4ac729f14d47
Simon Kågedal Reimer <skagedal@gmail.com>
2023-08-29 07:50:29 +0200

    Remove old shell functions (replaced by the tracker tool)

--- install.sh
+++ install.sh
@@ -9,8 +9,3 @@ BUILT_BINARY=`pwd`/build/install/${TOOL}/bin/${TOOL}
 ./gradlew install
 ln -fs ${BUILT_BINARY} ${BIN}/${TOOL}
 
-if [ -d ~/.oh-my-zsh ]; then
-    _SIMONS_ASSISTANT_COMPLETE=zsh ${BUILT_BINARY} > ~/.oh-my-zsh/custom/simons-assistant.zsh
-    ln -fs `pwd`/shell/simons-assistant.zsh ~/.oh-my-zsh/custom/simons-assistant-function.zsh
-fi
-
