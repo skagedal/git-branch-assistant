5ececfafac5f9c6ae7dc7c3da8ad003b25665436
Simon Kågedal Reimer <skagedal@gmail.com>
2024-01-01 23:11:27 +0100

    gradle: convert to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,29 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.20"
     application
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ben.manes.versions)
+    alias(libs.plugins.version.catalog.update)
 }
 
 repositories {
     mavenCentral()
 }
 
-object Versions {
-    val spring = "5.3.4"
-    val jackson = "2.11.0"
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.8")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", "1.3.8")
-    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", Versions.jackson)
-    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml", Versions.jackson)
-    implementation("com.fasterxml.jackson.core", "jackson-databind", Versions.jackson)
-    implementation("org.slf4j", "slf4j-api", "1.7.30")
-    implementation("ch.qos.logback", "logback-classic", "1.2.3")
-    implementation("com.github.ajalt", "clikt", "2.4.0")
-    implementation("de.codeshelf.consoleui", "consoleui", "0.0.13")
+    implementation(libs.logback.classic)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.jdk8)
+    implementation(platform(libs.jackson.bom))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation(libs.clikt)
+    implementation(libs.consoleui)
 
-    testImplementation("org.jetbrains.kotlin", "kotlin-test")
-    testImplementation("org.jetbrains.kotlin", "kotlin-test-junit5")
+    compileOnly(libs.slf4j.api)
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.0")
 }
 
